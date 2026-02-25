c7e3f057d13538425717c40b033e750016c66f5b
Iryna Hrytsenko <iryna.vhr@gmail.com>
2026-02-25 21:56:01 +0200

    fuctional of scraping added. debuging needed. some urls are strange

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import asyncio
 import asyncpg
 import os
 from dotenv import load_dotenv
-
+from scraper import fetch_start_page, extract_car_links, parse_car_page, fetch_car_page
 load_dotenv()
 
 
@@ -26,6 +26,7 @@ async def create_table(conn):
 
 
 async def main():
+    # connect to DB
     conn = await asyncpg.connect(
         user=os.getenv("POSTGRES_USER"),
         password=os.getenv("POSTGRES_PASSWORD"),
@@ -33,14 +34,28 @@ async def main():
         host=os.getenv("POSTGRES_HOST"),
         port=5432,
     )
-
     print("✅ DB connected")
 
+    # create table if not exists
     await create_table(conn)
-
     print("✅ Table created")
 
-    await conn.close()
+    
+    # parcing test
+    html = await fetch_start_page()
+    links = extract_car_links(html)
 
+    # print(links[:10])  
+
+    # fetch and parse first car page
+    # first_car_url = links[3]
+    first_car_url = "https://auto.ria.com/uk/auto_mercedes_benz_s_class_39172208.html"
+
+    car_html = await fetch_car_page(first_car_url)
+    car_data = parse_car_page(car_html, first_car_url)
+
+
+    # close connection 
+    await conn.close()
 
 asyncio.run(main())
---
+++ app/scraper.py
@@ -0,0 +1,160 @@
+import httpx
+from bs4 import BeautifulSoup
+import re
+
+
+
+START_URL = "https://auto.ria.com/uk/car/used/"
+
+
+async def fetch_start_page():
+    async with httpx.AsyncClient() as client:
+        response = await client.get(START_URL)
+
+    print("Status:", response.status_code)
+
+    return response.text
+
+
+def extract_car_links(html: str):
+    soup = BeautifulSoup(html, "html.parser")
+
+    links = []
+
+    # всі <a> теги з посиланнями
+    for a in soup.find_all("a", href=True):
+        href = a["href"]
+
+        # AutoRia карточки авто виглядають так:
+        # https://auto.ria.com/auto_....
+        if "/auto_" in href and "auto.ria.com" in href:
+            links.append(href)
+
+    print(f"Found {len(links)} links before deduplication")
+    # прибираємо дублікати
+    links = list(set(links))
+
+    print(f"Found {len(links)} car links")
+
+    return links
+
+
+async def fetch_car_page(url: str):
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url)
+
+    print(f"Opened car page: {response.status_code}")
+
+    return response.text
+
+
+
+
+def parse_car_page(html: str, url: str):
+    soup = BeautifulSoup(html, "html.parser")
+
+    # ---------------- TITLE ----------------
+    parent = soup.find(id="basicInfoTitle")
+    title_tag = parent.find("h1", class_="titleL")
+    title = title_tag.get_text(strip=True) if title_tag else None
+
+    # ---------------- PRICE USD ----------------
+    price = None
+    parent = soup.find(id="basicInfoPrice")
+    price_tag = parent.find("strong", class_="titleL")
+    if price_tag:
+        price_text = price_tag.get_text(strip=True)
+        price = int(re.sub(r"\D", "", price_text))
+
+    # ---------------- ODOMETER ----------------
+    odometer = None
+    parent = soup.find(id="basicInfoTableMainInfo0")
+    odometer_tag = parent.find("span", class_="body")
+
+    if odometer_tag:
+        text = odometer_tag.get_text(strip=True).lower()
+        number = int(re.search(r"\d+", text).group())
+        odometer = number * 1000 if "тис" in text else number
+        
+       
+
+    # ---------------- USERNAME ----------------
+    username = None
+    parent = soup.find(id="sellerInfoUserName")
+    seller_tag = parent.find("span", class_="common-text ws-pre-wrap titleM")
+    if seller_tag:
+        username = seller_tag.get_text(strip=True)
+
+    # ---------------- PHONE ----------------!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    phone_number = None
+    parent = soup.find(id="sellerInfo")
+    parent = parent.find("div", class_="button-main mt-12")
+    phone_tag = parent.find("span", class_="common-text ws-pre-wrap action")
+    if phone_tag:
+        text = phone_tag.get_text(strip=True)
+        phone_number = re.search(r"\d+", text).group()
+        if phone_number[:2] != "38":
+            phone_number = "38" + phone_number
+        phone_number = int(phone_number)
+   
+
+    # ---------------- IMAGE ----------------
+    image_url = None
+    parent = soup.find("span", class_="picture")
+    img_tag = soup.find("img")
+    if img_tag:
+        image_url = img_tag["data-src"]
+
+    # ---------------- IMAGES COUNT ----------------
+    images_count = 0
+    parent = soup.find(class_="common-badge alpha medium")
+    spans = parent.find_all("span")
+    nums = []
+    for span in spans:
+        text = span.get_text(strip=True)
+        if text.isdigit():
+            nums.append(int(text))
+    images_count = nums[1]
+
+    # ---------------- CAR NUMBER & VIN ----------------
+    car_number = None
+    parent = soup.find("div", class_="car-number")  
+    
+    car_number_tag = None
+    if parent:
+        car_number_tag = parent.find("span", class_="body")
+    if car_number_tag:
+        car_number = car_number_tag.get_text(strip=True)
+  
+
+    # VIN ----------------
+    car_vin = None
+    parent = soup.find("div", class_="badge-template")  
+    
+    car_vin_tag = None
+    if parent:
+        car_vin_tag = parent.find("span", class_="badge")
+    
+    if car_vin_tag:
+        car_vin = car_vin_tag.get_text(strip=True)
+    
+
+
+    data = {
+        "url": url,
+        "title": title,
+        "price_usd": price,
+        "odometer": odometer,
+        "username": username,
+        "phone_number": phone_number,
+        "image_url": image_url,
+        "images_count": images_count,
+        "car_number": car_number,
+        "car_vin": car_vin,
+    }
+
+    print("\nParsed car:")
+    for k, v in data.items():
+        print(k, "=", v)
+
+    return data
